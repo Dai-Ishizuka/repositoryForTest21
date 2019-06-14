bfd094ff7ce2e07b87c00ca2fb2a0cac03617575
Dai Ishizuka <tennisnx80s@i.softbank.jp>
2019-06-14 11:40:24 +0900

    firstCommit

---
+++ test21/test21/TopMainView.swift
@@ -0,0 +1,39 @@
+//
+//  TopMainView.swift
+//  test21
+//
+//  Created by 石塚大 on 2019/06/14.
+//  Copyright © 2019 石塚大. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+protocol TopMainViewDelegate: NSObjectProtocol{
+    
+}
+
+extension TopMainViewDelegate {
+    
+}
+// MARK: - Property
+class TopMainView: BaseView {
+    weak var delegate: TopMainViewDelegate? = nil
+}
+
+// MARK: - Life cycle
+extension TopMainView {
+    
+}
+
+// MARK: - Protocol
+extension TopMainView {
+    
+}
+
+// MARK: - method
+extension TopMainView {
+    
+}
+
+
---
+++ test21/test21/TopViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TopViewController.swift
+//  test21
+//
+//  Created by 石塚大 on 2019/06/14.
+//  Copyright © 2019 石塚大. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    
+    @IBOutlet weak var topMainView: TopMainView!
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    
+}
+
+// MARK: - Protocol
+extension TopViewController {
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
